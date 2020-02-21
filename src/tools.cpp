e78a310bed56a6585f35e552b65c0e0e9d550f9d
williamhyin <william.hyin@outlook.com>
2020-02-21 21:26:25 +0800

    first commit

---
+++ src/tools.cpp
@@ -0,0 +1,24 @@
+#include "tools.h"
+#include <iostream>
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  /**
+   * TODO: Calculate the RMSE here.
+   */
+}
+
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+  /**
+   * TODO:
+   * Calculate a Jacobian here.
+   */
+}
